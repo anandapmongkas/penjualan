35b27af0137546310e42ecbaa540fb1a14d55cad
unknown <anandapamongkas@gmail.comgit config --global user.name anandapmongkas>
2022-08-22 20:52:13 -0700

    upload ke github

---
+++ resources/views/Barang/index.blade.php
@@ -0,0 +1,64 @@
+@extends('layout.app')
+
+@section('title')
+    Barang
+@endsection
+
+@section('Content')
+<div class="card mt-3">
+    <div class="card-header">
+        <div class="card-title">
+        <h5>Data Barang</h5>
+
+        <button type="button" class="btn btn-success btn-sm float-end" data-bs-toggle="modal" data-bs-target="#ModalTambah"><i class="fa fa-plus"></i></button>
+        </div>
+    </div>
+
+</div>
+<div class="card-body">
+<table class="table table-striped mt-5">
+<thead>
+    <tr> 
+        <th>No.</th>
+        <th>Nama</th>
+        <th>Harga</th>
+        <th>Stok</th>
+        <th>suplier</th>
+        <th>Kategori</th>
+        <th>Aksi</th>
+</div>
+</thead>
+
+<tbody>
+    <tr>
+        <td>1</td>
+        <td>Video</td>
+        <td>20.000</td>
+        <td>1</td>
+        <td>Video</td>
+        <td>ATK</td>
+        <td>
+            <a href="#" class="btn btn-warning btn-sm"> <i class ="fa fa-edit"></i></a>
+            <a href="#" class="btn btn-danger btn-sm"> <i class ="fa fa-trash"></i></a>
+        </td>
+    </tr>
+
+<div class="modal fade" id="ModalTambah" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</table>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('layout.app');
+});
+
+Route::get('/barang', function () {
+    return view('Barang.index');
+});
+Route::get('/kategori', function () {
+    return view('kategori.index');
+});
+Route::get('/suplier', function () {
+    return view('suplier.index');
+});
+Route::get('/pembeli', function () {
+    return view('pembeli.index');
+});
+Route::get('/pembelian', function () {
+    return view('pembelian.index');
+});
+Route::get('/penjualan', function () {
+    return view('penjualan.index');
+});
